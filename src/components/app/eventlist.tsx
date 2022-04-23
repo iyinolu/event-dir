0e7bfe9c6f922d8b428005ac2ed464ae8057acba
Paul Osinowo <47259302+iyinolu@users.noreply.github.com>
2022-04-23 09:18:52 +0100

    test request

--- src/components/app/eventlist.tsx
+++ src/components/app/eventlist.tsx
@@ -134,7 +134,7 @@ export default function EventList() {
       <div className="flex flex-col rounded-xl bg-transparent h-full px-8 pt-0 pb-7 overflow-auto min-h-full w-full mt-[73px]">
         {showEvents(eventStateValue)}
       </div>
-      {eventStateValue[0]?.title}
+      <p className="text-red-800">{eventStateValue[0]?.title}</p>
       <viewEventContext.Provider value={{ viewEvent, setViewEvent }}>
         <ShowEvent />
       </viewEventContext.Provider>
